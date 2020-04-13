c757b601b0c109b10042b97c0da97903728707f8
Ctracey92 <cody.tracey92@gmail.com>
2020-04-13 13:59:56 -0500

    added in the next part to the example

--- watcher-spawn.js
+++ watcher-spawn.js
@@ -5,8 +5,23 @@ const filename = process.argv[2];
 if (!filename) {
   throw Error("A file to watch must be specified!");
 }
+
+// Version 1:
+// fs.watch(filename, () => {
+//     const ls = spawn('ls', ['-l', '-h', filename]);
+//     ls.stdout.pipe(process.stdout);
+// });
+
+//Version 2:
 fs.watch(filename, () => {
-    const ls = spawn('ls', ['-l', '-h', filename]);
-    ls.stdout.pipe(process.stdout);
+  const ls = spawn("ls", ["-l", "-h", filename]);
+  let output = "";
+
+  ls.stdout.on('data', chunk => output += chunk);
+
+  ls.on("close", () => {
+    const parts = output.split(/\s+/);
+    console.log([parts[0], parts[4], parts[8]]);
+  });
 });
 console.log(`Now watching ${filename} for changes...`);
